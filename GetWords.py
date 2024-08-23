3a7fbf812e37a3e745b3f773ca1369cb47fe9b7c
Siddhanth Lalgowdar <61350672+siddhanth78@users.noreply.github.com>
2024-08-22 23:16:18 -0600

    Update GetWords.py

--- GetWords.py
+++ GetWords.py
@@ -2,9 +2,11 @@ import json
 
 dictionary = json.loads(open("words_dictionary.json", 'r').read())
 
-words = set([d for d in dictionary if len(d) >= 3])
+words = set([d for d in dictionary if len(d) > 3])
 
-letter_set = set(list('undhekc'))
+letter_set = set(list('hapcred'))
+
+center_l = 'h'
 
 valid = []
 
@@ -14,6 +16,8 @@ def replace_char(w, c):
 
 for word in words:
     check = word
+    if center_l not in check:
+        continue
     for l in letter_set:
         check = replace_char(check, l)
     if check.strip() == '':
